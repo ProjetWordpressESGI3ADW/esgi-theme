bdb9b15e4a350338f04644a008eb238d59ed594f
Kintokih <tibohardelin@gmail.com>
2016-04-08 15:22:06 +0200

    Ajout des widgets

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ add_action('init', 'menus');
  */
 function wpdocs_theme_slug_widgets_init() {
     register_sidebar( array(
-        'name'          => __( 'Main Sidebar <3', 'esgi' ),
+        'name'          => __( 'Main Sidebar', 'esgi' ),
         'id'            => 'sidebar-1',
         'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'textdomain' ),
         'before_widget' => '<li id="%1$s" class="widget %2$s">',
@@ -33,6 +33,57 @@ function wpdocs_theme_slug_widgets_init() {
 	add_action( 'widgets_init', 'wpdocs_theme_slug_widgets_init' );
 
 /*
+*		############  WIDGET ############
+*/
+	function my_widgets(){
+		register_widget('link_custom');
+	}
+	add_action('widgets_init', 'my_widgets');
+	
+	/**
+	* 
+	*/
+	class link_custom extends WP_Widget
+	{
+		public function link_custom(){
+			parent::__construct(false, 'link_custom');
+			$options = array(
+				'classname'		=>	'link-custom', 
+				'description'	=>	'ceci est notre premier widget' 
+			);
+			$this->WP_Widget('link-custom', 'Lien personnalisé', $options);
+		}		
+		public function widget($args, $instance){
+			// Widget output
+			echo '<a href="'.$instance["url"].'" class="custom-url">'.$instance["name"].'</a>';
+		}
+		public function update($new, $old){
+			// Save widget options
+			return $new;
+		}
+		public function form($instance){
+			// Output admin widget options form
+			$params = array(
+				'url'	=> 'http://www.google.com',
+				'name'	=> 'GOOGLE'
+			);
+			$instance = wp_parse_args($instance, $params);
+
+			echo '
+				<p>
+					<label for="'.$this->get_field_id("name").'">Titre du lien : </label>
+					<input value="'.$instance["name"].'" name="'.$this->get_field_name("name").' id="'.$this->get_field_id("name").'" type="text">
+				</p>
+				<p>
+					<label for="'.$this->get_field_id("url").'">Url : </label>
+					<input value="'.$instance["url"].'" name="'.$this->get_field_name("url").' id="'.$this->get_field_id("url").'" type="text">
+				</p>
+			';
+		}
+	}
+
+
+/*
 	Ajout d'un Logo d'en-tete
 */
 
@@ -40,20 +91,50 @@ function wpdocs_theme_slug_widgets_init() {
 			'default-image'				=>	get_template_directory_uri().'/img/logo1.png',
 			'width'						=>	0,
 			'height'					=>	0,
-			'flex-height'				=>	0,
-			'flex-width'				=>	0,
-			'uploads'					=>	0,
-			'random-default'			=>	0,
-			'header-text'				=>	0,
-			'default-text-color'		=>	0,
-			'wp-head-callback'			=>	0,
-			'admin-head-callback'		=>	0,
-			'admin-preview-callback'	=>	0,
+			'flex-height'				=>	'',
+			'flex-width'				=>	'',
+			'uploads'					=>	'',
+			'random-default'			=>	'',
+			'header-text'				=>	'',
+			'default-text-color'		=>	'',
+			'wp-head-callback'			=>	'',
+			'admin-head-callback'		=>	'',
+			'admin-preview-callback'	=>	'',
 	);
 	add_theme_support('custom-header', $defaults);
 
-	function menu_page(){
-		add_menu_page('Option_suplementaire', 'Option Sup', 'administrator', 'manage_options');
-
-	}
+function menu_page(){
+	add_menu_page('Options_suplementaires', 'Option Sup', 'administrator', 'manage_options', 'options_page');
+}
 	add_action("admin_menu", "menu_page");
+
+/* modifications du theme importés dans la table wp_options */
+function theme_options(){
+	register_setting('esgi-theme', 'background');
+	register_setting('esgi-theme', 'text_color');
+}
+	add_action('admin_init', 'theme_options');
+
+/* Construction de la page d'options */
+function options_page(){
+	echo '<h1>Ma page d\'options</h1>
+			<form action="options.php" method="POST">';
+				settings_fields('esgi-theme');
+				echo'<label for="background">Background : </label>'
+				.'<input id="background" name="background" value="'.get_option('background').'" type="text">'
+				.'<label for="background">Couleur du texte : </label>'
+				.'<input id="text_color" name="text_color" value="'.get_option('text_color').'" type="text">'
+				.'<input value="Mettre à jour" type="submit">
+			</form>';
+}
+/* Styliser le head */
+function head_style(){
+	echo('<style>')
+			.'body{
+				background-color:'.get_option('background').';'
+				.'color:'.get_option('text_color').';
+			 }
+		 </style>';
+}
+add_action('wp_head', 'head_style');
+
