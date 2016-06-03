c794dc838eab75558806a0ff0ce3749669d906ec
Teddy Meksavanh <teddy@mobilispro.com>
2016-06-03 11:09:17 +0200

    Toggle

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,21 @@
 "use strict";
 
+jQuery( document ).ready(function() {
+	menu.init();
+});
+
 var menu = {	
-	init : function(){		
+	init : function(){				
+		/**
+		*	Setter
+		*/
 		menu.setOpenNavbarSide();
+		menu.setNavbarSide();
+		menu.setNavbarSideCollapse();
+
+		/**
+		*	Functions
+		*/
 		menu.toggle();
 	},
 
@@ -13,18 +26,40 @@ var menu = {
 		this._openNavbarSide = jQuery('#open-navbar-side-menu');
 	},
 
+	setNavbarSide : function(){
+		this._navbarSide = jQuery('.navbar-side-menu');
+	},
+
+	setNavbarSideCollapse : function(){
+		this._navbarSideCollapse = jQuery('.navbar-side-menu.navbar-side-menu-collapse');
+	},
+
 	/**
 	*	Getter
 	*/	
 	getOpenNavbarSide : function(){
 		return this._openNavbarSide;
 	},
-	toggle : function(){				
-		menu.getOpenNavbarSide().on('click', function(){				
-			console.log("test");
+
+	getNavbarSide : function(){
+		return this._navbarSide;
+	},
+
+	getNavbarSideCollapse : function(){
+		return this._navbarSideCollapse;
+	},
+
+	/**
+	*	Functions
+	*/
+	toggle : function(){	
+		menu.getOpenNavbarSide().on('click', function(){									
+            if(menu.getNavbarSide().hasClass('navbar-side-menu-collapse'))
+                menu.getNavbarSide().removeClass('navbar-side-menu-collapse');
+            else
+                menu.getNavbarSide().addClass('navbar-side-menu-collapse');                
 		});
 	}
 };
 
 
-menu.init();
