228bdd07682edcbe8f4bc75542bea3cff154e3f1
Kintokih <tibohardelin@gmail.com>
2016-06-13 09:52:34 +0200

    Ajout script liant boutton d'ajout de media à la modale wordpress d'ajout de media

--- js/admin.js
+++ js/admin.js
@@ -21,3 +21,19 @@ var dom = {
 		this.getEventLabel().text("Ajouter un nouvel Event ");
 	}
 }
+
+/** ACTIVATION MODALE POUR RECHERCHE MEDIA **/
+jQuery(document).ready(function() {
+
+	jQuery('#upload_image_button').click(function() {
+											  window.send_to_editor = function(html) {
+		 imgurl = jQuery('img',html).attr('src');
+		 jQuery('#upload_image').val(imgurl);
+		 tb_remove();
+			}
+
+
+	 tb_show('', 'media-upload.php?post_id=1&type=image&TB_iframe=true');
+	 return false;
+	});
+});
