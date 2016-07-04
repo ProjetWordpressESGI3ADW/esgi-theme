a64386217d29d78df1ac2214f15986b1c68350e1
Dylan <dylanfoster.esgi@gmail.com>
2016-07-04 11:58:13 +0200

    admin.js -> refacto JS popup upload d'image

--- js/admin.js
+++ js/admin.js
@@ -4,6 +4,7 @@ window.addEventListener('load', function load(){
 	//(on devrait faire ça idéalement pour tous les events utilisés une seule fois) 
 	window.removeEventListener('load', load, false);
 	dom.init();
+	rechercheMedia.init();
 });
 var dom = {
 	init: function(){
@@ -22,18 +23,36 @@ var dom = {
 	}
 }
 
+function isElSoloJqueryInstance(el){
+	if(el.length == 1 && el instanceof jQuery)
+		return true;
+	return false;
+}
+
 /** ACTIVATION MODALE POUR RECHERCHE MEDIA **/
-jQuery(document).ready(function() {
-
-	jQuery('#upload_image_button').click(function() {
-											  window.send_to_editor = function(html) {
-		 imgurl = jQuery('img',html).attr('src');
-		 jQuery('#upload_image').val(imgurl);
-		 tb_remove();
+var rechercheMedia ={
+	init: function(){
+		this.setUploadBtn();
+		if(isElSoloJqueryInstance(this.getUploadBtn()))
+			this.loadBtnEvent();
+		else
+			console.log("couldn't find upload btn");
+	},
+	setUploadBtn: function(){
+		this._upBtn = jQuery('#upload_image_button');
+	},
+	getUploadBtn: function(){
+		return this._upBtn;
+	},
+	loadBtnEvent: function(){
+		this.getUploadBtn().click(function() {
+			window.send_to_editor = function(html) {
+				imgurl = jQuery('img',html).attr('src');
+				jQuery('#upload_image').val(imgurl);
+				tb_remove();
 			}
-
-
-	 tb_show('', 'media-upload.php?post_id=1&type=image&TB_iframe=true');
-	 return false;
-	});
-});
+			tb_show('', 'media-upload.php?post_id=1&type=image&TB_iframe=true');
+			return false;
+		});
+	},
+}
