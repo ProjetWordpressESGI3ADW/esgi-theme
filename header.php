87ea83043a0591eaf8d3b4f40b66294d69e5f2e2
Dylan <dylanfoster.esgi@gmail.com>
2016-07-06 23:09:36 +0200

    navbar-x affichée en bas (pour l'instant), liens non dynamiques

--- header.php
+++ header.php
@@ -7,19 +7,57 @@
 	</head>
 	<body>
 		<header>
-			<nav class="navbar navbar-default navbar-fixed-top">
-				<div class="container-fluid">
-					<div class="navbar-header">
-				        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					        <span class="sr-only">Toggle navigation</span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-				      	</button>
-				      	<a class="navbar-brand" href="#"><?= bloginfo("name"); ?></a>
-				    </div>
-				</div>
-			</nav>
+			<?php if (has_nav_menu('x_menu')): ?>
+				<nav class="navbar navbar-default navbar-fixed-bottom">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class=" navbar-brand x-menu-logo-a " href=""><img class="x-menu-logo-img" src="<?php header_image(); ?>" alt="logo"></a>
+						</div>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="#">Events <span class="sr-only">(current)</span></a></li>
+								<li><a href="#">News</a></li>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Top 3 drawings <span class="caret"></span></a>
+									<ul class="dropdown-menu">
+										<li><a href="#">Drawing 1</a></li>
+										<li><a href="#">Drawing 2</a></li>
+										<li><a href="#">Drawing 3</a></li>
+									</ul>
+								</li>
+							</ul>
+							<form class="navbar-form navbar-right" role="search">
+								<div class="form-group">
+									<input type="text" class="form-control" placeholder="Search">
+								</div>
+								<button type="submit" class="btn btn-default">Submit</button>
+							</form>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			<?php else: ?>
+				<nav class="navbar navbar-default navbar-fixed-top">
+					<div class="container-fluid">
+						<div class="navbar-header">
+					        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						        <span class="sr-only">Toggle navigation</span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+					      	</button>
+					      	<a class="navbar-brand" href="#"><?= bloginfo("name"); ?></a>
+					    </div>
+					</div>
+				</nav>
+			<?php endif; ?>
 		</header>
 		<div class="container-fluid main-container">
 			<div class="row">
