a354727265bd0238d474417b6b5280b97f3aa9b2
Dylan <dylanfoster.esgi@gmail.com>
2016-07-06 23:10:23 +0200

    js/admin.js --> correction de la modificiation du titre d'articile: ne s'effectue plus que sur la seule page "evenement"

--- js/admin.js
+++ js/admin.js
@@ -9,17 +9,36 @@ window.addEventListener('load', function load(){
 var dom = {
 	init: function(){
 		this.setEventLabel();
-		if(this.getEventLabel() instanceof jQuery && this.getEventLabel().length==1 )
+		this.setEventGlobalMenu();
+		if(isElSoloJqueryInstance(this.getEventLabel()) && this.isUserOnEventPage()){
 			this.changeEventCreationLabel();
+			console.log("sur page event!");
+		}
 	},
 	setEventLabel: function(){
 		this._eventLabel = jQuery('#wpbody-content > div.wrap > h1');
 	},
+	setEventGlobalMenu: function(){
+		this._globEvMenu = jQuery('#menu-posts-event');
+	},
 	getEventLabel: function(){
 		return this._eventLabel;
 	},
+	getEventGlobalMenu: function(){
+		return this._globEvMenu;
+	},
+	isUserOnEventPage: function(){
+		if($_GET('post_type') == 'event') return true;
+		if(this.getEventGlobalMenu().hasClass('wp-menu-open')) return true;
+		return false;
+	},
 	changeEventCreationLabel: function(){
-		this.getEventLabel().text("Ajouter un nouvel event ");
+		console.log(this.getEventGlobalMenu().find('.wp-submenu li:last-child'));
+		if(!(this.getEventGlobalMenu().find('.wp-submenu li:last-child').hasClass('current'))){
+			this.getEventLabel().text("Ajouter un nouvel event ");
+			jQuery('#post').attr('enctype', 'multipart/form-data');
+		}
+		
 	}
 }
 
@@ -27,6 +46,21 @@ function isElSoloJqueryInstance(el){
 	if(el.length == 1 && el instanceof jQuery)
 		return true;
 	return false;
+}
+
+function $_GET(param) {
+	var vars = {};
+	window.location.href.replace( location.hash, '' ).replace( 
+		/[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+		function( m, key, value ) { // callback
+			vars[key] = value !== undefined ? value : '';
+		}
+	);
+
+	if ( param ) {
+		return vars[param] ? vars[param] : null;	
+	}
+	return vars;
 }
 
 /** ACTIVATION MODALE POUR RECHERCHE MEDIA **/
@@ -56,3 +90,6 @@ var rechercheMedia ={
 		});
 	},
 }
+
+
+/*edit.php?post_type=event*/
