a5dd3c91a5b5a167175d6073caf6c8bbfa072a80
Kintokih <tibohardelin@gmail.com>
2016-07-07 21:27:40 +0200

    test si deja voté

--- event.php
+++ event.php
@@ -22,13 +22,14 @@
 				// var_dump($resultats);
 				// var_dump((int)$resultats[0]->nb);
 				// // var_dump();
-				if((int) $resultats[0]->nb == 0 || is_null($resultats[0])){
+				if((int) $resultats[0]->nb == 0){
 					echo "tu peux enregistrer ton image maggle";
 
 					$query = "SELECT COUNT(email) as nb FROM {$wpdb->prefix}image WHERE name=".$name." LIMIT 0,1";
 					$resultats = $wpdb->get_results($query);
 					if((int) $resultats[0]->nb == 0){
 						$finalPath = renameContestEventImg($idpost, $newImg['path'], $name, $newImg['format']);
+						var_dump($finalPath);
 						// var_dump($wpdb->prefix.'image');
 
 						// var_dump($idpost);
@@ -78,32 +79,40 @@
 /* ##########  POST DE VOTE ########### */
 	else if(isset($_POST['vote_mail'])){
 		$mail = trim($_POST['vote_mail']);
-		if(isset($_POST['image_choose'])){
+		//On check si on a pas deja voté avec cet e-mail
+		$query = "SELECT COUNT(mail) as nb FROM {$wpdb->prefix}vote WHERE mail=".$mail." AND post=".$idpost." LIMIT 0,1";
+		$resultats = $wpdb->get_results($query);
+		var_dump($resultats);
+		if((int) $resultats[0]->nb == 0){
+			if(isset($_POST['image_choose'])){
 			$img = $_POST['image_choose'];
-			if(filter_var($mail,FILTER_VALIDATE_EMAIL)){		
-				$wpdb->insert(
-			    	$wpdb->prefix.'vote',
-				    array(
-				        'mail' => $mail,
-				        'image' => $img,
-				        'post' => $idpost,
-				    ),
-				    array(
-				        '%s',
-				        '%d',
-				        '%d',
-				    )
-				);
-				$e = 0;
+				if(filter_var($mail,FILTER_VALIDATE_EMAIL)){		
+					$wpdb->insert(
+				    	$wpdb->prefix.'vote',
+					    array(
+					        'mail' => $mail,
+					        'image' => $img,
+					        'post' => $idpost,
+					    ),
+					    array(
+					        '%s',
+					        '%d',
+					        '%d',
+					    )
+					);
+					$e = 0;
+				}else{
+					//Si adresse email éronnée 
+					$e = 1;
+				}	
 			}else{
-				//Si adresse email éronnée 
-				$e = 1;
-			}	
-		}else{
 			//Si pas d'image choisie
 			$ei = 2;
 		}
-		
+		}else{
+			//Si deja voté
+			$e = 4;
+		}
 	}else{
 	//Si pas d'adresse mail renseignée
 		$em = 3;
@@ -155,6 +164,7 @@
 					if($e == 1) echo '<span class="error">Adresse email invalide</span>';
 					if($e == 2) echo '<span class="error">Veuillez choisir une image</span>';
 					if($e == 3) echo '<span class="error">Renseignez l\'adresse email</span>';
+					if($e == 4) echo '<span class="error">Vous avez deja voté !</span>';
 			}
 		?>
 		<input type="submit">
@@ -174,11 +184,5 @@
 			</form>
 		</div>
 	</div>
-	
-	<h3>Coche une des images, inscris ton adresse email et vote pour ton dessin préféré!</h3>
-	<form class="formVoteDessin" action="<?php echo get_template_directory_uri()?>/eventvote_post.php" method="POST">
-		Email : <input placeholder="email@domaine.com" type="email" name="email">
-		<input class="enterEmail btn btn-primary" type="submit">
-	</form>	
 <?php
 
