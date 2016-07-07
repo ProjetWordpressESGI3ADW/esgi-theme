b6e48a6c7dc77d1dffbf7abca8eb733c2a391160
Dylan <dylanfoster.esgi@gmail.com>
2016-07-07 21:01:58 +0200

    correction de style sur la page type d'un event

--- event.php
+++ event.php
@@ -52,25 +52,25 @@
 						);
 						// var_dump($r);
 						if($r){
-							echo "GG T'AS UP TON DESSIN FRERO";
+							$msg= "GG T'AS UP TON DESSIN FRERO";
 						}
 						else{
-							echo "L'UP A FAIL !";
+							$msg= "L'UP A FAIL !";
 						}
 					}
 					else
-						echo "Ce nom d'image est déjà utilisé pour cet event !";
+						$msg= "Ce nom d'image est déjà utilisé pour cet event !";
 				}
 				else
-					echo "Une image a déjà été postée avec cet email !";
+					$msg= "Une image a déjà été postée avec cet email !";
 			}
 			else{
-				echo "image's name must me 49 characters long and be composed only of aplhanumerical characters !";
-				echo "<br>";
-				echo "email must be formated as: your_email@email.com";
+				$msg= "image's name must me 49 characters long and be composed only of aplhanumerical characters !";
+				$msg= "<br>";
+				$msg= "email must be formated as: your_email@email.com";
 			}
 		}else{
-			echo 'wtf3';
+			$msg= 'wtf3';
 		}
 
 		unset($_POST['event_proposed_email'], $_POST['event_proposed_img_name'], $_FILES['event_proposed_drawing']);flush();
@@ -125,50 +125,53 @@
 		$description = get_post_meta($post->ID, 'event_description');
 		$path = explode('/', $description[0]);
 		$path = get_template_directory_uri().'/images/event/'.$path[count($path)-1];
-		echo $path;
 	?>
-		
-	<div class="banniere">
-		<h1>Titre</h1>
-		<img src="https://sp.yimg.com/ib/th?id=OIP.M2de0ec74f51e3c8d313ffda88464b32eH0&pid=15.1&rs=1&c=1&qlt=95&w=105&h=108#inline">
-		<div>01/01/2001</div>
+		<?php if (isset($msg)): ?>
+			<div id="event_up_msg" class="animation fadeDown fixed display-flex-column"><p><a href=""><?php echo $msg; ?></a></p></div>
+		<?php endif ?>
+	<div class="banniere display-flex-column">
+		<h1><?php echo $post->post_title; ?></h1>
+		<img id="event_banner_img" src="<?php echo $imgPath; ?>">
+		<div class="display-flex-column" id="event_datefin">
+			<p>Ending date: <?php echo get_post_meta($post->ID, 'event_datefin')[0]; ?></p>
+		</div>
 	</div>
 	
 	<div class="description">
-		description description description description description description description description description description description description 
-		description description description description description description description description description description description description 
-		description description description description description description description description description description description description 
-		description description description description description description description description description description description description 
+		<p><?php echo $description[0]; ?></p>
 	</div>
 
-	<h3>Coche une des images, inscris ton adresse email et vote pour ton dessin préféré!</h3>
-	<form action="" method="POST">
-		<input type="radio" name="image_choose" value="1">
-		<input type="radio" name="image_choose" value="2">
-		<input type="radio" name="image_choose" value="3">
-		<input type="radio" name="image_choose" value="4">
-		<input type="radio" name="image_choose" value="5">
-		<input type="email" name="vote_mail" placeholder="Votre e-mail">
-		<?php 
-			if(isset($e)){
-					if($e == 0) echo '<span class="success">Vote effectué !</span>';
-					if($e == 1) echo '<span class="error">Adresse email invalide</span>';
-					if($e == 2) echo '<span class="error">Veuillez choisir une image</span>';
-					if($e == 3) echo '<span class="error">Renseignez l\'adresse email</span>';
-			}
-		?>
-		<input type="submit">
-	</form>
-	<div class="fixed" id="toi-aussi-upload-ton-img">
-		<div id="popup" class="reduire">Masquer</div>
-		<h3 class="titre_popup">Toi aussi up ton dessin miskin</h3>
+	
+	<div class="display-flex-column">
+		<h3>Vote for your favorite drawing !</h3>
+		<form action="" method="POST">
+			<input type="radio" name="image_choose" value="1">
+			<input type="radio" name="image_choose" value="2">
+			<input type="radio" name="image_choose" value="3">
+			<input type="radio" name="image_choose" value="4">
+			<input type="radio" name="image_choose" value="5">
+			<input type="email" name="vote_mail" placeholder="Votre e-mail">
+			<?php 
+				if(isset($e)){
+						if($e == 0) echo '<span class="success">Vote effectué !</span>';
+						if($e == 1) echo '<span class="error">Adresse email invalide</span>';
+						if($e == 2) echo '<span class="error">Veuillez choisir une image</span>';
+						if($e == 3) echo '<span class="error">Renseignez l\'adresse email</span>';
+				}
+			?>
+			<input type="submit">
+		</form>
+	</div>
+	<div class="fixed display-flex-column" id="toi-aussi-upload-ton-img">
+		<div id="popup" class="reduire">Hide</div>
+		<h3 class="titre_popup">Propose your drawing too</h3>
 		<div id="img-upload-form-container">
 			<form enctype="multipart/form-data" action="" method="post">
-				<label for="event_proposed_drawing">Slit here</label>
+				<label for="event_proposed_drawing">There would be a good place to put your image in, it has to be a png, jpg or jpeg though</label>
 				<input required class="img-responsive" id="event_proposed_drawing" name="event_proposed_drawing" type="file">
-				<label for="event_proposed_img_name">Donnez un nom à votre image !</label>
+				<label for="event_proposed_img_name">What name should your image have ?</label>
 				<input type="text" name="event_proposed_img_name">
-				<label for="event_proposed_email">Donne ton email pour recevoir </label>
+				<label for="event_proposed_email">We're gonna need your email though, in case you win ;)</label>
 				<input required type="email" id="event_proposed_email" name="event_proposed_email" placeholder="email@domain.com">
 				<input class="btn btn-primary" type="submit" value="Envoyer !">
 			</form>
@@ -176,7 +179,7 @@
 	</div>
 	
 	<h3>Coche une des images, inscris ton adresse email et vote pour ton dessin préféré!</h3>
-	<form class="formVoteDessin" action="<?php echo get_template_directory_uri()?>/eventvote_post.php" method="POST">
+	<form class="formVoteDessin display-flex-column" action="<?php echo get_template_directory_uri()?>/eventvote_post.php" method="POST">
 		Email : <input placeholder="email@domaine.com" type="email" name="email">
 		<input class="enterEmail btn btn-primary" type="submit">
 	</form>	
